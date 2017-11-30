710a57c9eb1b79d44ec5241b1e4ed3a528598e41
Evan Harmon <evan.p.harmon@gmail.com>
2017-11-29 20:00:41 -0800

    feat(thunk): add json-server and implement thunk middleware fetch

--- src/reducers/note.js
+++ src/reducers/note.js
@@ -1,24 +1,29 @@
 import { EditorState } from 'draft-js';
+import getNotes from '../lib/noteServices';
 
 const initState = {
-  notes: [
-    { id: 0, title: 'note 0', text: 'this is text for note 0' },
-    { id: 1, title: 'note 1', text: 'this is text for note 1' },
-    { id: 2, title: 'note 2', text: 'this is text for note 2' },
-  ],
+  notes: [],
   currentNote: { id: 2, title: 'note 2', text: 'this is text for note 2' },
   editorState: EditorState.createEmpty(),
 };
 
 const NOTE_ADD = 'NOTE_ADD';
 const EDITOR_UPDATE = 'EDITOR_UPDATE';
+const NOTES_LOAD = 'NOTES_LOAD';
 
 export const updateEditor = val => ({ type: EDITOR_UPDATE, payload: val });
+export const loadNotes = notes => ({ type: NOTES_LOAD, payload: notes });
+export const fetchNotes = () => async dispatch => {
+  const notes = await getNotes();
+  dispatch(loadNotes(notes));
+};
 
 export default (state = initState, { type, payload }) => {
   switch (type) {
     case NOTE_ADD:
       return { ...state, notes: [...state.notes, payload] };
+    case NOTES_LOAD:
+      return { ...state, notes: payload };
     case EDITOR_UPDATE:
       return { ...state, editorState: payload };
     default:
