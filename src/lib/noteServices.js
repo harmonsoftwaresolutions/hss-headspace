2952e35c1a335c18ef08ae0c3c1106be9cc3f8b0
Evan Harmon <evan.p.harmon@gmail.com>
2017-11-30 23:13:03 -0800

    feat(redux): add to note list working

--- src/lib/noteServices.js
+++ src/lib/noteServices.js
@@ -1,4 +1,18 @@
-export default async () => {
+export const getNotes = async () => {
   const res = await fetch('http://localhost:8080/notes');
+
+  return res.json();
+};
+
+export const createNote = async title => {
+  const res = await fetch('http://localhost:8080/notes', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ title, text: '' }),
+  });
+
   return res.json();
 };
--- src/reducers/note.js
+++ src/reducers/note.js
@@ -1,29 +1,39 @@
 import { EditorState } from 'draft-js';
-import getNotes from '../lib/noteServices';
+import { getNotes, createNote } from '../lib/noteServices';
 
 const initState = {
   notes: [],
-  currentNote: { id: 2, title: 'note 2', text: 'this is text for note 2' },
+  currentNote: '',
   editorState: EditorState.createEmpty(),
 };
 
-const NOTE_ADD = 'NOTE_ADD';
 const EDITOR_UPDATE = 'EDITOR_UPDATE';
 const NOTES_LOAD = 'NOTES_LOAD';
+const NOTE_ADD = 'NOTE_ADD';
+const CURRENT_UPDATE = 'CURRENT_UPDATE';
 
 export const updateEditor = val => ({ type: EDITOR_UPDATE, payload: val });
 export const loadNotes = notes => ({ type: NOTES_LOAD, payload: notes });
+export const addNote = note => ({ type: NOTE_ADD, payload: note });
+export const updateCurrent = val => ({ type: CURRENT_UPDATE, payload: val });
+
 export const fetchNotes = () => async dispatch => {
-  const notes = await getNotes();
-  dispatch(loadNotes(notes));
+  const res = await getNotes();
+  dispatch(loadNotes(res));
+};
+export const saveNote = title => async dispatch => {
+  const res = await createNote(title);
+  dispatch(addNote(res));
 };
 
 export default (state = initState, { type, payload }) => {
   switch (type) {
-    case NOTE_ADD:
-      return { ...state, notes: [...state.notes, payload] };
     case NOTES_LOAD:
       return { ...state, notes: payload };
+    case NOTE_ADD:
+      return { ...state, currentNote: '', notes: [...state.notes, payload] };
+    case CURRENT_UPDATE:
+      return { ...state, currentNote: payload };
     case EDITOR_UPDATE:
       return { ...state, editorState: payload };
     default:
