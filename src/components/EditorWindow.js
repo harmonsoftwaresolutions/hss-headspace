7b73cbfa53545d278ff09c545c585e5a157b7441
Evan Harmon <evan.p.harmon@gmail.com>
2017-12-21 09:00:22 -0800

    feat(editor): tie in readOnly initial editor state

--- src/components/EditorWindow.js
+++ src/components/EditorWindow.js
@@ -4,25 +4,32 @@ import PropTypes from 'prop-types';
 import { Editor } from 'draft-js';
 import * as fromEditor from '../reducers/editor';
 
-const EditorWindow = ({ editorState, saveEditor }) => (
+const EditorWindow = ({ editorState, readOnly, saveEditor }) => (
   <div id="editor">
-    <Editor editorState={editorState} onChange={saveEditor} />
+    <Editor
+      editorState={editorState}
+      onChange={saveEditor}
+      readOnly={readOnly}
+    />
   </div>
 );
 
 EditorWindow.defaultProps = {
   editorState: PropTypes.shape({}),
+  readOnly: PropTypes.bool,
   saveEditor: PropTypes.func,
 };
 
 EditorWindow.propTypes = {
   editorState: PropTypes.shape({}),
+  readOnly: PropTypes.bool,
   saveEditor: PropTypes.func,
 };
 
 export default connect(
   state => ({
     editorState: state.editor.editorState,
+    readOnly: state.editor.readOnly,
   }),
   {
     saveEditor: fromEditor.saveEditor,
